c7a1a0187ac1528291e8ce68446cd7b33c37eaeb
Steven Luu <stevmac@Stevens-MacBook-Pro.local>
2022-03-01 00:55:46 -0600

    hambuger menu

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,12 @@
 console.log("js linked!")
+
+document.addEventListener("DOMContentLoaded", () =>{
+    const nav = document.querySelector(".navs");
+    document.querySelector("#buttonNav").addEventListener("click", () =>{
+        nav.classList.add("nav--open");
+    });
+
+    document.querySelector(".nav-overlay").addEventListener("click", () =>{
+        nav.classList.remove("nav--open");
+    });
+});
